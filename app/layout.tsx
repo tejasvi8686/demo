9e2bec10155dde5b5e0258a8ff02a355493edd9f
tejasvi@appeneure <tejasviraj8686@gmail.com>
2025-03-30 01:04:53 +0530

    added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from '@/components/theme-provider';
+import { ThemeProvider } from "@/components/theme-provider";
 // import { Toaster } from "@/components/ui/sonner";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Frontend Developer Portfolio',
-  description: 'Portfolio showcasing frontend development skills and projects',
+  title: "Frontend Developer Portfolio",
+  description: "Portfolio showcasing frontend development skills and projects",
 };
 
 export default function RootLayout({
@@ -18,11 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        {children}
-        {/* <Toaster /> */}
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          {/* <Toaster /> */}
         </ThemeProvider>
       </body>
     </html>
